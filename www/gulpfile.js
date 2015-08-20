01182029269b95e2fc2e851302091cfd34f5067c
Ryan <ryankanno@localkinegrinds.com>
2015-08-20 06:31:26 -1000

    Fixed watch:styles task to watch multiple changes

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -69,17 +69,17 @@ function compile(watch) {
 function watch() { return compile(true); };
 
 function styles() {
-  gulp.src(paths.styles.src_files)
-      .pipe(sourcemaps.init())
-      .pipe(sass({
-          outputStyle: 'compressed',
-          sourceComments: 'map',
-          includePaths : [paths.styles.src]
-      }))
-      .on('error', function(err) { console.error(err); this.emit('end'); })
-      .pipe(autoprefix('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-      .pipe(rename(paths.styles.dst_filename))
-      .pipe(gulp.dest(paths.styles.dst))
+  return gulp.src(paths.styles.src_files)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed',
+        sourceComments: 'map',
+        includePaths : [paths.styles.src]
+    }))
+    .on('error', function(err) { console.error(err); this.emit('end'); })
+    .pipe(autoprefix('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(rename(paths.styles.dst_filename))
+    .pipe(gulp.dest(paths.styles.dst))
 }
 
 function watch_styles() {
@@ -91,4 +91,4 @@ gulp.task('clean', function(cb) { return clean(cb); });
 gulp.task('styles', function(cb) { return styles(); });
 gulp.task('watch', function(cb) { return watch(); });
 gulp.task('watch:styles', function(cb) { return watch_styles(); });
-gulp.task('default', ['clean', 'watch', 'watch:styles']);
+gulp.task('default', ['clean', 'watch:styles']);
